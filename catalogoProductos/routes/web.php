01944f76647b03a09e414b6d06b2876c74dc270f
Alberto Lopez <albertojo44.al@gmail.com>
2021-02-17 16:25:38 -0600

    listo

--- catalogoProductos/routes/web.php
+++ catalogoProductos/routes/web.php
@@ -14,22 +14,3 @@
 Route::get('/', function () { //LLAMA EL ARCHIVO PHP
     return view('welcome');
 });
-
-Route::post('/catalogo', function(){
-    return view('catalogo.catalogo');         //LLAMA EL ARCHIVO PHP
-});
-
-Route::get( '/catalogo/{producto?}/{precio?}', function ($producto = "Pantalla lcd", $precio = null) { //VALORES POR DEFECTO
-    // return view('catalogo', array( //RETORNA VALORES AL PHP                                  
-    //     "producto" => $producto,
-    //     "precio" => $precio
-    // ));ç
-    return view('catalogo.catalogo')->with('producto', $producto)
-                                    ->with('precio', $precio)
-                                    ->with('elementos',array('RAM','CAMARA','MONITOR','DISCO DURO','TECLADO'));
-})->where([     //CONDICIONES                        
-    'producto' => '[A-Za-z]+',   
-    "precio" => '[0-9]+'
-]);
-
-Route::get('/catalogofunction','catalogoController@index');
